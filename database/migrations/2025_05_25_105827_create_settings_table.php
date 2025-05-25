2f673ebf765b88071e0994658eee8d46d67f2477
Charaf Oualid <542269@student.fontys.nl>
2025-05-25 13:16:00 +0200

    Migrated Settings and added Seeder

---
+++ database/migrations/2025_05_25_105827_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('logo_path')->nullable();
+            $table->string('font')->nullable();
+            $table->string('nav_color')->default('bg-gray-800');
+            $table->string('button_color')->default('bg-indigo-600');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
