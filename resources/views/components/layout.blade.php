88c6f4a03ad038ee6b7eb0c983b1a01c29587a68
Charaf Oualid <542269@student.fontys.nl>
2025-05-30 13:31:27 +0200

    last cmt

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,7 +23,7 @@
                         <div class="ml-10 flex items-baseline space-x-4">
                             <x-nav-link href="{{ route('index') }}" :active="request()->routeIs('index')">Bazaar</x-nav-link>
                             <x-nav-link href="{{ route('orders.index') }}" :active="request()->routeIs('orders.index')">Bestelgeschiedenis</x-nav-link>
-                            <x-nav-link href="{{ route('rented.show') }}" :active="request()->routeIs('rented.show')">Gehuurde producten</x-nav-link>
+                            <x-nav-link href="{{ route('rented.show') }}" :active="request()->routeIs('rented.show')">Gehuurde prod.</x-nav-link>
 
 
                             <!-- Alleen zichtbaar voor adverteerders -->
